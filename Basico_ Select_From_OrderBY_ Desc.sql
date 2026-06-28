1867a213a43c051023657aedeb94b737a15fb3db
Joao Oliveira <joao.mpoliveira29@gmail.com>
2026-06-28 16:07:28 -0300

    Primeira Aula

---
+++ Basico_ Select_From_OrderBY_ Desc.sql
@@ -0,0 +1,11 @@
+/* FUNÇÃO SELECT E FROM */
+
+SELECT*
+FROM Person.Person
+
+/* Selecione a coluna BusinessEnitityID da tabela Person.Person e 
+classifique em ordem decressente*/
+
+SELECT BusinessEntityID
+FROM Person.Person
+ORDER BY BusinessEntityID desc
